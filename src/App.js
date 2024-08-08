569588d28fab4c96d5e499a3f39dc1f4e1618813
Niggha2003 <nguyennghia241003@gmail.com>
2024-08-08 17:32:19 +0700

    Khởi tạo dự án và cài đặt các thư viện cần thiết

--- src/App.js
+++ src/App.js
@@ -1,25 +1,28 @@
-import logo from './logo.svg';
 import './App.css';
+import * as React from 'react';
+import DeleteIcon from '@mui/icons-material/Delete';
+import IconButton from '@mui/material/IconButton';
+import Tooltip from '@mui/material/Tooltip';
+import { Typography } from '@mui/material';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    return (
+        <div className="App">
+            <Tooltip
+                title={
+                    <React.Fragment>
+                        <Typography color="inherit">Tooltip with HTML</Typography>
+                        <em>{"And here's"}</em> <b>{'some'}</b> <u>{'amazing content'}</u>.{' '}
+                        {"It's very engaging. Right?"}
+                    </React.Fragment>
+                }
+            >
+                <IconButton>
+                    <DeleteIcon />
+                </IconButton>
+            </Tooltip>
+        </div>
+    );
 }
 
 export default App;
